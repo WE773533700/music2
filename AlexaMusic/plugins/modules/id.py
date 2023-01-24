2737bb8b9e42fe2d70c3f62f7a2fdea4e283de0d
WRTRTYYIFOPU <123384890+WRTRTYYIFOPU@users.noreply.github.com>
2023-01-24 22:42:40 +0300

    Add files via upload

--- AlexaMusic/plugins/modules/id.py
+++ AlexaMusic/plugins/modules/id.py
@@ -4,7 +4,7 @@ from AlexaMusic import app
 from pyrogram import filters
 
 
-@app.on_message(filters.command("id"))
+@app.on_message(filters.command("ايدي","ا","id"],"))
 def ids(_, message):
     reply = message.reply_to_message
     if reply:

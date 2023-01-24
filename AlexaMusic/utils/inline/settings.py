cdb05fbb85c5901c06fd4ee52d1aad642844a3bf
WRTRTYYIFOPU <123384890+WRTRTYYIFOPU@users.noreply.github.com>
2023-01-24 22:37:11 +0300

    Add files via upload

--- AlexaMusic/utils/inline/settings.py
+++ AlexaMusic/utils/inline/settings.py
@@ -30,8 +30,8 @@ def setting_markup(_):
         ],
         [
             InlineKeyboardButton(
-                text="• Sᴜᴩᴩᴏʀᴛ •",
-                url="https://t.me/Shayri_Music_Lovers",
+                text="« الـدعـم »",
+                url="https://t.me/FH_KP",
             ),
             InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
         ],
